c9f72a513383a56711d588301215ffa91084786c
equasoda <haachch@gmail.com>
2019-09-06 10:37:48 +0800

    '修改'

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     '/': {
       lang: 'zh-CN',
       title: '前端知识点总结',
-      description: '任何不能够给你快乐的编程都是耍流氓'
+      description: ' '
     }
   },
   head: [
@@ -29,12 +29,12 @@ module.exports = {
 
     docsDir: 'src',
     editLinks: true,
-   
+
     locales: {
       '/': {
         label: '简体中文',
         repo: 'equasoda/study',
-      
+
         selectText: '选择语言',
         editLinkText: '帮助我们改进内容',
         lastUpdated: '上次更新',
@@ -46,40 +46,40 @@ module.exports = {
           {
             text: '算法',
             items: [
-              { text: '编程基本功', link: '/zh/algorithm/Charpter4.md'},
-              { text: '数学趣题(一)', link: '/zh/algorithm/Charpter5.md'},
-              { text: '数学趣题(二)', link: '/zh/algorithm/Charpter6.md'}
+              { text: '编程基本功', link: '/zh/algorithm/Charpter4.md' },
+              { text: '数学趣题(一)', link: '/zh/algorithm/Charpter5.md' },
+              { text: '数学趣题(二)', link: '/zh/algorithm/Charpter6.md' }
             ],
           },
           {
             text: '项目相关',
             items: [
-              { text: '前端规范', link: '/zh/standard/Standard.md'},
-              { text: '项目流程', link: '/zh/standard/Project.md'},
-              { text: '前后端交互', link: '/zh/standard/Cooperation.md'},
-              { text: '移动端兼容问题', link: '/zh/standard/Compatibility.md'},
-              { text: '项目优化', link: '/zh/standard/Improve.md'},
-              { text: '单页面应用', link: '/zh/standard/Spa.md'}
+              { text: '前端规范', link: '/zh/standard/Standard.md' },
+              { text: '项目流程', link: '/zh/standard/Project.md' },
+              { text: '前后端交互', link: '/zh/standard/Cooperation.md' },
+              { text: '移动端兼容问题', link: '/zh/standard/Compatibility.md' },
+              { text: '项目优化', link: '/zh/standard/Improve.md' },
+              { text: '单页面应用', link: '/zh/standard/Spa.md' }
             ],
           },
           {
             text: '知名公司面试题',
             items: [
-              { text: '面试必看', link: '/zh/interview/index.md'},
-              { text: '阿里巴巴', link: '/zh/interview/ali.md'},
-              { text: '龙湖地产', link: '/zh/interview/longhu.md'}
+              { text: '面试必看', link: '/zh/interview/index.md' },
+              { text: '阿里巴巴', link: '/zh/interview/ali.md' },
+              { text: '龙湖地产', link: '/zh/interview/longhu.md' }
             ]
           },
           {
             text: '知识点笔记',
             items: [
-              { text: '在线思维导图', link: '/zh/interface/Mind.md'},
+              { text: '在线思维导图', link: '/zh/interface/Mind.md' },
               { text: 'Vue', link: '/zh/vue/' },
-              { text: 'React', link: '/zh/react/'},
-              { text: '小程序', link: '/zh/miniapp/' },
-              { text: '悦读管理后台', link: '/zh/interface/Yuedu.md'},
-              { text: 'BlackMarket', link: '/zh/interface/BlackMarket.md'},
-              { text: 'TouTiao', link: '/zh/interface/TouTiao.md'}
+              { text: 'React', link: '/zh/react/' }
+              // { text: '小程序', link: '/zh/miniapp/' },
+              // { text: '悦读管理后台', link: '/zh/interface/Yuedu.md'},
+              // { text: 'BlackMarket', link: '/zh/interface/BlackMarket.md'},
+              // { text: 'TouTiao', link: '/zh/interface/TouTiao.md'}
             ]
           },
           // {
@@ -100,24 +100,30 @@ module.exports = {
         ],
         sidebar: {
           '/guide/': genSidebarConfig('指南'),
-          '/zh/ali/': [{title:'阿里面试题'}],
-          '/zh/vue/': [{title: 'Vue知识点', children: [
-            '', 'Es6', 'Es6方法扩展', 'Webpack', 'Babel', 'Promise', 'Vue 各种方法之间的区别', 'Vue组件', 'Vue路由', 'Vue动画', 'vue钩子函数', 'vuex'
-          ]}],
-          '/zh/react/': [{title: 'React知识点', children: [
-            '', 'JSX', 'MVVM', 'Props', 'React生命周期', 'React组件', 'React路由', 'redux', 'redux中间件', '组件通信', '高阶组件', 'dom diff'
-          ]}],
-          '/zh/miniapp/': [{title: '小程序知识点', children: [
-            '', 'WXS', 'wepy组件通信', '小程序事件', '小程序路由', '小程序组件', '小程序组件化'
-          ]}]
+          '/zh/ali/': [{ title: '阿里面试题' }],
+          '/zh/vue/': [{
+            title: 'Vue知识点', children: [
+              '', 'Es6', 'Es6方法扩展', 'Webpack', 'Babel', 'Promise', 'Vue 各种方法之间的区别', 'Vue组件', 'Vue路由', 'Vue动画', 'vue钩子函数', 'vuex'
+            ]
+          }],
+          '/zh/react/': [{
+            title: 'React知识点', children: [
+              '', 'JSX', 'MVVM', 'Props', 'React生命周期', 'React组件', 'React路由', 'redux', 'redux中间件', '组件通信', '高阶组件', 'dom diff'
+            ]
+          }],
+          '/zh/miniapp/': [{
+            title: '小程序知识点', children: [
+              '', 'WXS', 'wepy组件通信', '小程序事件', '小程序路由', '小程序组件', '小程序组件化'
+            ]
+          }]
         }
       }
     },
-    
+
   }
 }
 
-function genSidebarConfig (title) {
+function genSidebarConfig(title) {
   return [
     {
       title,
